e89144ae422f65f5b8ababeffdc3ae5189188db8
Daniel Perez <tuvistavie@gmail.com>
2014-02-16 06:10:34 +0900

    Add sound settings for push notification.

--- app/lib/push_manager.rb
+++ app/lib/push_manager.rb
@@ -13,6 +13,9 @@ class PushManager
     notification = Houston::Notification.new(device: token)
     notification.custom_data = data.reject { |k| k.to_sym == :token }
     notification.content_available = 1
+    if data.has_key? :position
+      notification.sound = (data[:position] & 0x04) == 0 ? 'migi.mp4' : 'hidari.mp4'
+    end
     @apn.push(notification)
   end
 
